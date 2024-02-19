e7ca0c53e3142c2e86b7d2616fcd28c3e167aa4e
ashish dadhich <ashishdadhich098@gmail.com>
2024-02-19 13:43:24 +0530

    project.execution.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -78,15 +78,3 @@ class ModelTrainer:
             logging.info('Exception occured at Model Training')
             raise CustomException(e,sys)
         
-
-#Wrote this to check code is running correctly
-
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-    model_trainer=ModelTrainer()
-    model_trainer.initiate_model_training(train_arr,test_arr)
-
-    
